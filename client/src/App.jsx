fed38187e8fbf59f4501aafd390c96aa270d98e0
Palash Hawee <haweepalash@gmail.com>
2024-10-16 21:53:57 +0600

    set up files

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // Optional: Import your CSS file
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+//db connection
+
+mongoose.connect("mongodb+srv://haweepalash:QCs3NPCUMog87wqn@cluster0.eblfz.mongodb.net/",
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to database'))
+.catch((error) => console.log('Connection error:', error));
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: [
+        'content-type',
+        'Authorization',
+        'Cache-Control',
+        'Expires',
+        'Pragma',
+    ],
+    credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT,()=>console.log(`listening on port ${PORT}`));
